ebab8c3ff99e76692247f410bf910575057c3fde
FreedyOmar <132398985+FreedyOmar@users.noreply.github.com>
2025-09-25 21:28:58 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-document.getElementById("loginForm").addEventListener("submit", function(event){
+// Animación de la carta
+window.addEventListener("load", () => {
+  const stage = document.querySelector('.stage');
+  stage.classList.add('open');
+
+  // Después de 8 segundos, cambiar texto por video
+  setTimeout(() => {
+  document.getElementById('texto').style.display = 'none';
+  
+  const videoEl = document.getElementById('video');
+  videoEl.style.display = 'block';
+  
+  // Reproducir automáticamente al aparecer
+  const vid = videoEl.querySelector("video");
+  vid.play().catch(err => {
+    console.log("El navegador bloqueó el autoplay:", err);
+  });
+}, 20000); 
+});
+
+// Login
+document.getElementById("loginForm")?.addEventListener("submit", function(event){
   event.preventDefault();
 
   let username = document.getElementById("username").value;
@@ -10,7 +31,7 @@ document.getElementById("loginForm").addEventListener("submit", function(event){
   const pass = "1234";
 
   if(username === user && password === pass){
-    alert("✅ Le damos la bienvenida a la chica con los ojos y la sonrisa mas hermosa");
+    alert("✅ Le damos la bienvenida a la chica con la mirada y la sonrisa mas hermosa");
     window.location.href = "inicio.html"; // redirige si es correcto
   } else {
     errorMsg.textContent = "❌ Usuario o contraseña incorrectos";
